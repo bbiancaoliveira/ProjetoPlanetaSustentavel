d58083efb91ffe9d887e20d653774e39036b3701
bbiancaoliveira <bbiancaoliveira@gmail.com>
2020-10-25 13:10:14 -0300

    atualizando pagina de receitas

---
+++ script.js
@@ -0,0 +1,12 @@
+
+				$(document).ready(function(){
+			   
+				   $('#link').on('change', function () {
+						var url = $(this).val(); 
+						if (url) { 
+							window.open(url, '_blank');
+						 }
+						 return false;
+					   });
+					});
+			   
